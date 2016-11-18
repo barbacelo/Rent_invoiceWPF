ab025ad66c772a1b67137163e52c1d062c844a48
barbacelo <Kavurma@KAVURMA-PC>
2016-11-18 13:58:18 +0100

    Add project files.

---
+++ WpfApplication3/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace WpfApplication3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        reversiEntities context = new reversiEntities();
+        CollectionViewSource kupciViewSource;
+        CollectionViewSource racuniViewSource;
+        CollectionViewSource robaViewSource;
+        public MainWindow()
+        {
+            InitializeComponent();
+            kupciViewSource = ((CollectionViewSource)(FindResource("kupciViewSource")));
+            robaViewSource = ((CollectionViewSource)(FindResource("robaViewSource")));
+            racuniViewSource = ((CollectionViewSource)(FindResource("racuniViewSource")));
+            DataContext = this;           
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            context.kupci.Load();
+            context.racuni.Load();
+            context.roba.Load();
+
+            kupciViewSource.Source = context.kupci.Local;
+            robaViewSource.Source = context.roba.Local;
+            racuniViewSource.Source = context.racuni.Local;            
+        }
+    }
+}
