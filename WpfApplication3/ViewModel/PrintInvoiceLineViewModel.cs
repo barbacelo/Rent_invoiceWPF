ebb8a2c60020266417674a537c2c1ce4b01cbacf
Barbacelo <milantarotcar@gmail.com>
2017-06-09 14:05:43 +0200

    Adding reports

---
+++ WpfApplication3/ViewModel/PrintInvoiceLineViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication3.ViewModel
+{
+    public class PrintInvoiceLineViewModel : ViewModelBase
+    {
+        public string Roba { get; set; }
+        public int Amount { get; set; }
+        public int Price { get; set; }
+    }
+}
