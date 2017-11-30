1e50cd390a5d6fcd95a454d082fa22a566f196a7
Barbacelo <milantarotcar@gmail.com>
2017-11-30 12:15:54 +0100

    Added enter key textbox traversal

--- WpfApplication3/App.xaml.cs
+++ WpfApplication3/App.xaml.cs
@@ -2,6 +2,8 @@
 using log4net;
 using System;
 using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace WpfApplication3
 {
@@ -23,6 +25,8 @@ namespace WpfApplication3
             AppDomain.CurrentDomain.UnhandledException +=
                 (s, ex) => log.Fatal($"AppDomain.CurrentDomain Exception: {ex.ExceptionObject}");
 
+            EventManager.RegisterClassHandler(typeof(TextBox), TextBox.KeyDownEvent, new KeyEventHandler(TextBox_KeyDown));
+
             void DomainUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
             {
                 var exception = unhandledExceptionEventArgs.ExceptionObject as Exception;
@@ -34,5 +38,30 @@ namespace WpfApplication3
                 ex.Handled = true;
             }
         }
+
+        void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter & (sender as TextBox).AcceptsReturn == false) MoveToNextUIElement(e);
+        }
+
+        void MoveToNextUIElement(KeyEventArgs e)
+        {
+            // Creating a FocusNavigationDirection object and setting it to a
+            // local field that contains the direction selected.
+            FocusNavigationDirection focusDirection = FocusNavigationDirection.Next;
+
+            // MoveFocus takes a TraveralReqest as its argument.
+            TraversalRequest request = new TraversalRequest(focusDirection);
+
+            // Gets the element with keyboard focus.
+            UIElement elementWithFocus = Keyboard.FocusedElement as UIElement;
+
+            // Change keyboard focus.
+            if (elementWithFocus != null)
+            {
+                if (elementWithFocus.MoveFocus(request)) e.Handled = true;
+            }
+        }
+
     }
 }
